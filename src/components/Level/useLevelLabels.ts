360dcbab8933e9be6274b470aabbeef4f719a160
martin-hillford <171671237+martin-hillford@users.noreply.github.com>
2025-06-16 12:26:17 +0200

    Added a couple components Improvement package publishing

--- src/components/Level/useLevelLabels.ts
+++ src/components/Level/useLevelLabels.ts
@@ -1,13 +1,6 @@
 import { useAvailableLanguage } from "../../hooks/useAvailableLanguage";
 import { terms } from "./terms";
 
-export const Level = (props : {value : string}) => {
-  const language = useAvailableLanguage();
-  const item = Object.keys(terms[language]).find(key => key === props.value)
-  if (!item) return <>{terms[language].Unspecified}</>;
-  return <>{(terms[language] as any)[item]}</>;
-};
-
 export const useLevelLabels = () => {
   const language = useAvailableLanguage();
   return [
--- src/components/Role/useRoleLabels.ts
+++ src/components/Role/useRoleLabels.ts
@@ -1,14 +1,6 @@
 import { useAvailableLanguage } from "../../hooks/useAvailableLanguage";
 import { terms } from "./terms";
 
-export const Role = ({ value } : { value : string | null | undefined }) => {
-  const language = useAvailableLanguage();
-  const item = Object.keys(terms[language]).find(key => key === value)
-  if (!item) return <span>{terms[language].Unknown}</span>;
-  return <span>{(terms[language] as any)[item]}</span>;
-};
-
-
 export const useRoleLabels = () => {
   const language = useAvailableLanguage();
   return [
